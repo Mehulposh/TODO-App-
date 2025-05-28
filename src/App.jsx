7d00e9398ac407da7389afc8bb4988a4ac6e208c
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-28 18:38:55 +0530

    added textfield button and functionality for loading and storing the todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import {Box,Stack,TextField,Button} from '@mui/material';
+import  { v4 as uuidv4 } from 'uuid';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [todoList, setTodoList] = useState([]);
+ const [input,setInput] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+ useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(todoList));
+  },[todoList])
+
+
+  useEffect(() => {
+    const prevTodo = JSON.parse(localStorage.getItem('list'));
+    setTodoList(prevTodo);
+  },[])
+
+
+  const handleAdd = () =>{
+    if(input.trim() !== ''){
+      const newItem = {
+        title: input.trim(),
+        id: uuidv4(),
+      };
+
+      setTodoList([...todoList,newItem]);
+      setInput('');
+    }
+  };
+
+ return (
+  <Box>
+    <Stack>
+      <TextField
+        placeholder= 'add item...'
+        multiline
+        type='text'
+        value={input}
+        onChange= {(e) => setInput(e.target.value)}
+        required
+        sx={{
+            width: 500,
+            mb: 2
+        }}
+      />
+      <Button
+        variant='contained'
+        onClick={handleAdd}
+        sx={{
+          width: '70px'
+        }}
+      >
+        Add
+      </Button>
+    </Stack>
+  </Box>
+ )
+
+  
 }
 
 export default App
